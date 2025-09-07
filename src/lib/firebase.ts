7dd65bf1a07d02f5d6c31e34e24331c89caee920
akubilayozel <84475288+akubilayozel@users.noreply.github.com>
2025-09-07 17:34:27 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,16 @@
-const firebaseConfig = {
-apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+import { getApps, initializeApp, getApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
+};
 
 export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const db = getFirestore(app);
